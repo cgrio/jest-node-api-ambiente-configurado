9f8424bdd04171fb29c423f43ba9a1690d00ca38
Alessandro Silva <alessrio@gmail.com>
2022-06-07 01:32:26 -0300

    Usando MockProxy

---
+++ tests/data/services/facebook-authentication.usandoSutMockProxy.spec.ts
@@ -0,0 +1,31 @@
+import { AuthenticationError } from '@/main/config/domain/errors'
+import { LoadFacebookUserApi } from '@/data/contracts/apis'
+import { FacebookAuthenticationService } from '@/data/services'
+
+import { mock, MockProxy } from 'jest-mock-extended'
+
+describe('MockProxy FacebookAuthenticationService (Use Case) Usando MockProxy', () => {
+  let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
+  let sut: FacebookAuthenticationService
+
+  beforeEach(() => {
+    loadFacebookUserApi = mock()
+    sut = new FacebookAuthenticationService(loadFacebookUserApi)
+  })
+
+  it('Chamando usando o Jest mockando a LoadFace...Spy correto', async () => {
+    await sut.perform({ token: 'any_token' })
+
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
+  })
+
+  it('MockProxy Mesmo teste anterior usando mock do jest undefined', async () => {
+    loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+
+    const authResult = await sut.perform({ token: 'any_token' })
+
+    expect(authResult).toEqual(new AuthenticationError())
+  })
+})
